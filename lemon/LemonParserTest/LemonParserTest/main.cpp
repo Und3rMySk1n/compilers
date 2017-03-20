e2d7c27287be77ed60f2d52f4ac36e84db0ec46c
und3rmysk1n <undermyskin@inbox.ru>
2017-03-20 19:31:45 +0300

    Прикрутил Flex вместо самописного лексера.

--- lemon/LemonParserTest/LemonParserTest/main.cpp
+++ lemon/LemonParserTest/LemonParserTest/main.cpp
@@ -1,36 +1,52 @@
 #include "stdafx.h"
 #include "CalcParser.h"
-#include "CalcLexer.h"
 #include "Token.h"
 #include <iostream>
 
+typedef struct yy_buffer_state *YY_BUFFER_STATE;
+
+extern "C"
+{
+	int tokenValue;
+	int yylex();
+	YY_BUFFER_STATE yy_scan_string(const char *yy_str);
+	void yy_delete_buffer(YY_BUFFER_STATE);
+}
+
 bool ParseExpr(std::string const& expr)
 {
-    CCalcLexer lexer(expr);
+	/// Строка ниже включает отладочный вывод LALR-парсера.
+	// parser.StartDebugTrace(stderr);
+
     CCalcParser parser;
 
-    /// Строка ниже включает отладочный вывод LALR-парсера.
-    // parser.StartDebugTrace(stderr);
+	char *inputString = new char[expr.length() + 1];
+	strcpy(inputString, expr.c_str());
+	yy_scan_string(inputString);
 
     SToken token;
-    int tokenId = 0;
+    int tokenId;
     do
     {
-        tokenId = lexer.Scan(token);
+        tokenId = yylex();
+		token.value = tokenValue;
+
         if (!parser.Advance(tokenId, token))
         {
             return false;
         }
     }
     while (tokenId != 0);
+	delete(inputString);
+
     return true;
 }
 
 int main()
 {
     std::string line;
-    while (std::getline(std::cin, line))
-    {
+    while (std::getline(std::cin, line))	
+	{
         if (!ParseExpr(line))
         {
             return 1;
@@ -39,3 +55,7 @@ int main()
 
     return 0;
 }
+
+
+// Чтобы вернуть структуру, которая содержит код токена и значение цифры,
+// можно использовать atoi(yytext)
